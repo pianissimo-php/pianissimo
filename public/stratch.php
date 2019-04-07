617b788f6df46c8ef89e115fbdcfef432b07e8d3
jonmldr <33514542+jonmldr@users.noreply.github.com>
2019-04-07 15:55:08 +0200

    update stratch file

--- public/stratch.php
+++ public/stratch.php
@@ -3,6 +3,7 @@
 /**
  * Returns all Controller classes
  */
+/*
 private function findControllerClasses(): array
 {
     //$match = preg_match('/Controller\\\\(.*)Controller/', $class);
@@ -12,6 +13,7 @@ private function findControllerClasses(): array
         IndexController::class,
     ];
 }
+*/
 
 /*
 if (isset($_SERVER['QUERY_STRING'])) {
