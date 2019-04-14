ce1b12629ce9d27cef8c538b5dcca9a6624a78b4
jonmldr <33514542+jonmldr@users.noreply.github.com>
2019-04-14 20:29:04 +0200

    minor change

--- Pianissimo/Component/HttpFoundation/Response.php
+++ Pianissimo/Component/HttpFoundation/Response.php
@@ -6,5 +6,8 @@ use GuzzleHttp\Psr7\Response as GuzzleResponse;
 
 class Response extends GuzzleResponse
 {
-
+    public function __construct(int $status = 200, array $headers = [], $body = null, string $version = '1.1', ?string $reason = null)
+    {
+        parent::__construct($status, $headers, $body, $version, $reason);
+    }
 }
